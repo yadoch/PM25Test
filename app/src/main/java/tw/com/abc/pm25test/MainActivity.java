038c17b00b0258f2962e303b982f65c9eaa2b97c
yadoch <yadoch@gmail.com>
2017-10-14 08:59:29 +0800

    完成版,有出現error 應該是Json 某一筆資料沒有pm2.5 資料

--- app/src/main/java/tw/com/abc/pm25test/MainActivity.java
+++ app/src/main/java/tw/com/abc/pm25test/MainActivity.java
@@ -1,8 +1,9 @@
 package tw.com.abc.pm25test;
 
-import android.Manifest;
 import android.content.Context;
-import android.support.v4.content.ContextCompat;
+import android.graphics.Color;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,37 +12,37 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
+
 import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedInputStream;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpCookie;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
+
 import java.net.URL;
 import java.net.URLConnection;
-import java.sql.Connection;
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+
 public class MainActivity extends AppCompatActivity {
+    private UIHandler handler;
     private ListView LvPm25;
     private BaseAdapter ListViewAdapter;
     private String jsonString;
     private JSONArray root;
     private List<Map<String,Object>> data;
-    private String[] from={"titxxxx","pm25xxx"};
-    private int[] to={R.id.tvcity,R.id.tvpm25};
+//    private String[] from={"titxxxx","pm25xxx"};
+//    private int[] to={R.id.tvcity,R.id.tvpm25};
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,9 +50,15 @@ public class MainActivity extends AppCompatActivity {
 
         LvPm25=(ListView)findViewById(R.id.lvpm25);
 
+        handler =new UIHandler();
         fetchData();
-
     }
+
+
+   public void Refresh(View view){
+       fetchData();
+   //    handler.sendEmptyMessage(0);
+   }
 
     private void fetchData() {
         new Thread(){
@@ -80,34 +87,21 @@ public class MainActivity extends AppCompatActivity {
         }
         try {
             root = new JSONArray(jsonString);
-/*
-//用 LinkedList 方式只能顯示最後一筆,且無法用lenght() 和 position 控制顯示內容
-
-            data = new LinkedList<>();
-            String city,pm25;
-            for (int i=0 ; i < root.length();i++) {
+            for (int i=0 ; i < root.length();i++){
                 JSONObject row = root.getJSONObject(i);
-                Map<String, Object> d0 = new HashMap<>();
-                d0.put(from[0], row.getString("County") + row.getString("SiteName"));
-                d0.put(from[1], row.getString("PM2.5"));
-                data.add(d0);
-            }
 
-            for(Map<String, Object> dItem:data){
-                city=dItem.get("titxxxx").toString();
-                pm25=dItem.get("pm25xxx").toString();
-                Log.i("geoff","city:"+city+"|PM2.5:"+pm25);
+                String country = row.getString("County");
+                String sitename = row.getString("SiteName");
+                String pm25 = row.getString("PM2.5");
+                Log.i("geoff",country + ":" + sitename + ":"+ pm25);
+
             }
-*/
         } catch (JSONException e) {
             //e.printStackTrace();
             Log.i("geoff",e.toString());
         }
-
-        LvPm25.setAdapter(new Myadapter(root));
-
+        handler.sendEmptyMessage(0);
     }
-
 
     private class Myadapter extends BaseAdapter {
         private JSONArray  MyjsonArray;
@@ -129,13 +123,13 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public long getItemId(int position) {
-            return 0;
+            return position;
         }
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
 
-            if(convertView == null){
+            //if(convertView == null){
                 //convertView= LayoutInflater.from(getApplicationContext()).inflate(R.layout.layoutitem, null);
                 convertView=inflater.inflate(R.layout.layoutitem,null);
                 TextView tvCity =(TextView)convertView.findViewById(R.id.tvcity);
@@ -146,24 +140,46 @@ public class MainActivity extends AppCompatActivity {
                     row = MyjsonArray.getJSONObject(position);
                     String city =row.getString("County")+row.getString("SiteName");
                     String pm25 =row.getString("PM2.5");
+                    double intpm25=Double.parseDouble(pm25);
                     tvCity.setText(city);
                     tvPm25.setText(pm25);
-                    
+                    if (  intpm25 > 0 && intpm25 <= 15.4){
+                        tvPm25.setBackgroundColor(Color.GREEN);
+                        tvCity.setBackgroundColor(Color.GREEN);
+                    }else if(intpm25 >= 15.5 && intpm25 <=35.4){
+                        tvPm25.setBackgroundColor(Color.YELLOW);
+                        tvCity.setBackgroundColor(Color.YELLOW);
+                    }else if(intpm25 >= 35.5 && intpm25 <=54.4) {
+                        tvPm25.setBackgroundColor(Color.parseColor("#FFA500"));
+                        tvCity.setBackgroundColor(Color.parseColor("#FFA500"));
+                    }else if(intpm25 >= 54.5 && intpm25 <=150.4) {
+                        tvPm25.setBackgroundColor(Color.RED);
+                        tvCity.setBackgroundColor(Color.RED);
+                    }else if(intpm25 >= 150.5 && intpm25 <=250.4) {
+                        tvPm25.setBackgroundColor(Color.parseColor("#800080"));
+                        tvCity.setBackgroundColor(Color.parseColor("#800080"));
+                    }else if(intpm25 >= 250.5) {
+                        tvPm25.setBackgroundColor(Color.parseColor("#A52A2A"));
+                        tvCity.setBackgroundColor(Color.parseColor("#A52A2A"));
+                    }
                 } catch (JSONException e) {
+                    Log.i("geoff", "Exception: " + e.toString());
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    Log.i("geoff", "Exception: " + e.toString());
                     e.printStackTrace();
                 }
-
-                /*
-                for(Map<String, Object> dItem:data){
-                    tvCity.setText(dItem.get("titxxxx").toString());
-                    tvPm25.setText(dItem.get("pm25xxx").toString());
-                }
-                */
-//                Log.i("geoff","root.length:"+root.length());
-
-            }
+            //}
             return convertView;
         }
     }
+    private class UIHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            LvPm25.setAdapter(new Myadapter(root));
+        }
+    }
+
 }
 
